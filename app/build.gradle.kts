1571968260af0f226077d9a041dfa7e1eb974330
Ugursal Ozan ARIK <ozanarik06@gmail.com>
2023-12-27 17:41:07 +0300

    Dagger hilt, Retrofit, Navigation Component, Data Store dependency implementations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -44,4 +46,28 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
+    //RETROFIT
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.google.code.gson:gson:2.10")
+
+    //LIVEDATA,LIFECYCLE
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+
+    //HILT
+    implementation("com.google.dagger:hilt-android:2.49")
+    implementation("com.google.dagger:hilt-compiler:2.49")
+
+    //DATA STORE
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+
+    //NAVIGATION
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
+
+
+
 }
